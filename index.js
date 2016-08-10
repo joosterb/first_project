2f126d8218667d039acce851b8294baf6058dd11
joosterb <johnoosterbroek@home.nl>
2016-08-10 12:19:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //$(document).ready(function(){
   alert('hello');
   var myEl = document.getElementById('Submit');
+  alert(myEl.innerHTML)
 
   myEl.addEventListener('click', function() {
     alert('Hello world');}, false);
